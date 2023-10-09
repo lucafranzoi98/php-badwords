a1215659b2a83b91efa257450edb5d3063c5a164
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-10-09 23:50:58 +0200

    initial commit
    form created

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Bad words</title>
+</head>
+<body>
+
+   <form action="./script.php" method="$_GET">
+      <input type="text" name="paragraph" id="paragraph">
+      <input type="text" name="badWord" id="badWord">
+      <button type="submit">Send</button>
+   </form>
+
+</body>
+</html>
